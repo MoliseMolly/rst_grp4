4c63c374a2ba162220431ee906fb4991848b850b
jona74c2 <jonasfriis94@gmail.com>
2019-10-15 23:03:29 +0200

    Added some comments in the code, css and html validated og adjusted. A few elementpositions is slightly changed, and the header is fixed

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", start);
-console.log("begin");
 
 function start() {
+    //link til streamers indsættes i et array. Ved at oprette links programatisk, kan det nye vindue åbnes som en ny tab, så brugeren ikke kommer væk fra sitet. Gjordes dette gennem et link i html, åbnes linket i samme tab, hvilket ikke ønskes her
     let streams = ["http://www.twitch.tv/marckozhd/", "http://www.twitch.tv/jk_fifa/", "http://www.twitch.tv/fjallefar/", "http://www.twitch.tv/brianfromdenmark/", "http://www.twitch.tv/jaxstyle/"];
+    //Streamernes html elementer læses ind i et array
     let streamerList = document.querySelectorAll(".streamer_item");
+
+    //Eventlisteners til knapper oprettes
     document.querySelector("#burgermenu").addEventListener("click", burger);
     document.querySelector("#read_article").addEventListener("click", readArticle);
     document.querySelector("#test").addEventListener("click", function () {
-        // Example on opening a new tab with link in js: https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window-using-javascript
+        // Eksempel på at åbne et link i nyt tab i js: https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window-using-javascript
         let win = window.open("https://play.kahoot.it/v2/?quizId=b6da4f54-0005-465d-82b6-97f1329553a6",
             '_blank');
         win.focus();
     });
 
+    //For hvert html streamer element oprettes en eventlistener der åbner linksene fra arrayet streams i nyt tab
     streamerList.forEach(function (item, i) {
         item.addEventListener("click", function () {
             // Example on opening a new tab with link in js: https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window-using-javascript
@@ -25,9 +29,10 @@ function start() {
 
 function burger() {
     console.log("we here");
-    //this code we have partly stolen from this example: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
-    var x = document.querySelector(".header_menu");
-    if (x.style.display === "block") {
+    //Basiskoden til at åbne og lukke burgermenuen er lånt herfra: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
+    //Når viewet er småt er header_menu skjult og ændret fra top menu, til en åben burgermenu. Klikkes der på burgermenuen sættes header_menu til synligt, og er den synlig, sættes den til skjult. Burgermenu billedet skifter mellem burgermenu og luk burgermenu
+    let x = document.querySelector(".header_menu");
+    if (x.style.display == "block") {
         x.style.display = "none";
         document.querySelector("#burgermenu").style.backgroundImage = "url(img/burger.svg)";
         document.querySelector("#burgermenu").style.width = "20px";
@@ -38,6 +43,7 @@ function burger() {
     }
 }
 
+//Når der klikkes på knappen Open Pimp My Ride, så fjernes knappen, gradienten fjernes, og artiklens højde sættes til auto, hvilket gør at hele artiklen vises i stedet for at være skjult
 function readArticle() {
     this.style.display = "none";
     document.querySelector(".grey_gradient").style.display = "none";
